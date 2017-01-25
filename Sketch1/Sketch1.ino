3c7b1244195b1d02604b82c4124b043181788587
juergmaier <juerg.maier@pobox.ch>
2017-01-25 17:59:19 +0100

    initial upload

---
+++ Sketch1/Sketch1.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Sketch1.ino
+ Created:	1/25/2017 5:57:23 PM
+ Author:	juerg.maier
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
